ae280e0871124edb288eb73f70452ffbc84800df
Marissa Flynn <mkflynn13@gmail.com>
2022-03-21 16:50:38 -0400

    Enabled connection to .env file and connected to sequelize.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,10 +1,17 @@
+// config function reads from the .env file.
+// requiring dotenv makes it have access to .env variables.
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// What are the question marks doing
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PW, 
+    {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
