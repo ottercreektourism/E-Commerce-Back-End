e87c3b7f8ec8659f3066bb5aef748c12e95f51ce
Marissa Flynn <mkflynn13@gmail.com>
2022-03-28 21:37:49 -0400

    Fixed seeding force error.

--- Develop/server.js
+++ Develop/server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // force true drops and recreates the Category table if it already exists regardless of whether theres stuff in it
 // TODO: Dont want to have this when I push it to heroku
-sequelize.sync({force: true}).then(() => {
+sequelize.sync({force: false}).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
